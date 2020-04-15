313288162ff77aabdd32a535470731349ab1ec58
nicktav4 <58477973+nicktav4@users.noreply.github.com>
2020-04-15 19:32:43 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 const Footer = ({ siteTitle }) => (
   <div className="footer">
     <span className="block">
-      &copy; {new Date().getFullYear()} {siteTitle}
+      &copy; {new Date().getFullYear()} {siteTitle} by Nicholas Taverna
     </span>
     <span className="block">
       <small>
